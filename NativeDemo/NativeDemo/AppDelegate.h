87ce358199d1a4dfe10a621a356b31b0cf695d10
hank <hank@Hank.lan>
2023-01-08 22:01:05 +0800

    添加工程

---
+++ NativeDemo/NativeDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NativeDemo
+//
+//  Created by hank on 2023/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NativeDemo/NativeDemo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  NativeDemo
+//
+//  Created by hank on 2023/1/8.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
